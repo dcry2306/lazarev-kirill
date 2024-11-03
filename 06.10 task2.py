ec1f518727e26dbf04e986f2435c89855aa032c0
dcry2306 <147155497+dcry2306@users.noreply.github.com>
2024-11-03 11:38:44 +0300

    Update 06.10 task2.py

--- 06.10 task2.py
+++ 06.10 task2.py
@@ -1,4 +1,24 @@
-num1 = float(input('Введите первое число: '))
-num2 = float(input('Введите второе число: '))
-mean = ((num1 + num2)/2)
-print('Среднее арифметическое -', round(mean))
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+
+RF24 radio(9, 8); // CE, CSN
+const byte address[6] = "000001"; // адрес платы приема
+const char message[] = "vzlom a1"; // сообщение для отправки при обнаружении света
+
+void setup() {
+  pinMode(A0, INPUT); // пин фоторезистора
+  Serial.begin(9600);
+  radio.begin();
+  radio.openWritingPipe(address);
+  radio.stopListening();
+}
+
+void loop() {
+  int lightValue = analogRead(A0);
+  if (lightValue < 920) { // регулируйте это значение в зависимости от чувствительности вашего фоторезистора
+    radio.write(message, sizeof(message));
+    Serial.println("Сообщение отправлено!");
+  }
+  delay(1000);
+}
